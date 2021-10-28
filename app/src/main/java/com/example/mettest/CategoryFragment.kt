fd58b8de2c457558c12edd9e9bc1fe8bfc35822d
Pijush <pijusmanna12344@gmail.com>
2021-10-28 21:08:24 +0530

    added layouts

--- app/src/main/java/com/example/mettest/CategoryFragment.kt
+++ app/src/main/java/com/example/mettest/CategoryFragment.kt
@@ -5,18 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.mettest.databinding.FragmentCategoryBinding
 
 
 class CategoryFragment : Fragment() {
-
     private lateinit var binding: FragmentCategoryBinding
+    private val viewModel:MainViewModel by activityViewModels()
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentCategoryBinding.inflate(inflater,container,false)
-        binding.textView2.text = arguments?.getString("ID") ?: "Not Found"
+       
+        arguments?.getString("ID")?.also { arg ->
+            viewModel.data.observe(viewLifecycleOwner,{ it1 ->
+                val formattedList = it1.filter {
+                    it.catId == arg
+                }
+                binding.textView2.text = formattedList[0].catName
+            }) 
+        }
+        
         return binding.root
     }
 }
